284b41580ab939df2abde7a42b46071758f17827
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-04-07 17:47:34 -0600

    Se agrega toda la lógica de la bitácora + se modulariza más el código. Ahora cuando se corre el programa, se crea una carpeta logs (si no existe) y en esta se guardan todos los requests que se le dan al servidor. Close #1

--- web_server.py
+++ web_server.py
@@ -1,8 +1,9 @@
-import os, threading, time, xml.etree.ElementTree as ET
+import os, threading, datetime, logging, time, xml.etree.ElementTree as ET
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from os import path
 from socketserver import ThreadingMixIn
 from io import BytesIO
+from urllib.parse import urlparse 
 
 class XML_Handler:
     root = None
@@ -40,12 +41,22 @@ class Server(BaseHTTPRequestHandler):
         self.send_header('Content-Type', content_type)
         self.end_headers()  
 
+    def log_info(self, command, server, referer='', url='', data=''):
+        logging.info(" {} > {} > {} > {} > /{} > {}".format(command, int(time.time()), server, referer, url, data))    
+
+    def get_args(self, url):
+        long_url = urlparse(self.path)
+        path = long_url[2] #path index
+        query = long_url[4] #query index
+
+        return path[1:], query
+        
     def do_HEAD(self):
-        if self.check_file(self.path[1:]):
-
-            print("Path: ", self.path)
-
-            file_to_open = open(self.path[1:],'rb').read() # rb lo abre como binario para que las imagenes funcionen
+        self.path, query = self.get_args(self.path)
+        self.log_info(self.command , 'localhost', '', self.path, query)
+        
+        if self.check_file(self.path):
+            file_to_open = open(self.path,'rb').read() # rb lo abre como binario para que las imagenes funcionen
 
             _, suffix = path.splitext(self.path)
             
@@ -53,26 +64,19 @@ class Server(BaseHTTPRequestHandler):
             content_length = len(file_to_open)
 
             self.set_headers(content_type, content_length)
-
         
     def do_GET(self):
-        print("GET request")
+        query = ''
 
         if self.path == '/':
-            self.path = '/index.html'
+            self.path = 'index.html'
         else:
-            current_path = self.path
-            current_path = current_path.split('?')
-            """
-            if len(current_path)>1:
-                link = current_path[1].split('&')
-                msg = link[0].split('=')
-                msg = msg[1]
-            """
-            self.path = current_path[0]
+            self.path, query = self.get_args(self.path)
 
-        if self.check_file(self.path[1:]):
-            file_to_open = open(self.path[1:],'rb').read() # rb lo abre como binario para que las imagenes funcionen
+        self.log_info(self.command, 'localhost', '', self.path, query)
+
+        if self.check_file(self.path):
+            file_to_open = open(self.path,'rb').read() # rb lo abre como binario para que las imagenes funcionen
 
             _, suffix = path.splitext(self.path)
             
@@ -83,9 +87,12 @@ class Server(BaseHTTPRequestHandler):
             self.wfile.write(bytes(file_to_open))
     
     def do_POST(self):
+        
+        self.path, query = self.get_args(self.path)
+        self.log_info(self.command, 'localhost', '', self.path, query)
         try:
-            print("POST request")
-            file_to_open = open(self.path[1:],'rb').read()
+            
+            file_to_open = open(self.path,'rb').read()
             
             _prefix, suffix = path.splitext(self.path)
             
@@ -102,17 +109,30 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 
 def main():
     port = 80
+    date_tag = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M-%S")
+    cwd = os.getcwd()
+    directory = os.path.join(cwd,"logs")
+    
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    
+    file_name = '{}/logs/bitacora_{}.log'.format(cwd, date_tag)
+
+    logging.basicConfig(filename=file_name, level=logging.INFO)
+
     try:
+        logging.info('\n---------\nLog started on %s.\n---------' % time.asctime())
+
         server = ThreadedHTTPServer(('localhost', port), Server)
-        #server = HTTPServer(('localhost', port), Server)
         print("Server running on port %s" % port)
         server.serve_forever()
+    
     except KeyboardInterrupt:
         print ("Closing web server")
+        logging.info('\n---------\nLog closed on %s.\n---------\n' % time.asctime())
         server.socket.close()
 
 if __name__ == '__main__':
     main()    
-
 
 
