4e1060c6a799748fb691d656c02835194bb79fbb
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-04-08 05:19:01 -0600

    Se soluciona el chequeo con el accept. El primer proceso es ahora el encargado de logear el inicio del logging. Close #8

--- web_server.py
+++ web_server.py
@@ -33,153 +33,152 @@ class XML_Handler:
                 return mime.find('mime-type').text
 
 class Server_Handler:
-	types_handler = XML_Handler('mimetypes.xml')
-	def check_file(self, file_path):
-		if os.path.exists(file_path):
-			return True
-		return False    
+    types_handler = XML_Handler('mimetypes.xml')
 
-	def look_for(self, looking, array):
-		index = 0
-		for item in array:
-			if looking in item:
-				return index
-			index+=1
-		return -1
-	def check_accept(self,data_vector):
+    def check_file(self, file_path):
+        if os.path.exists(file_path):
+            return True
+        return False    
 
-		file_name = data_vector[1]
-		accept_types = data_vector[3]
+    def look_for(self, looking, array):
+        index = 0
+        for item in array:
+            if looking in item:
+                return index
+            index+=1
+        return -1
+    
+    def check_accept(self,data_vector):
+        file_name = data_vector[1]
+        accept_types = data_vector[3]
 
-		if file_name == '/':
-			file_name = '/index.html'
+        if file_name == '/':
+            file_name = '/index.html'
 
+        _, suffix = os.path.splitext(file_name)
 
-		_, suffix = os.path.splitext(file_name)
+        if suffix.find('?') != -1: 
+            file_true_type = suffix.split("?")
+            suffix = file_true_type[0]
 
-		if suffix.find('?') != -1: 
-			file_true_type = suffix.split("?")
-			suffix = file_true_type[0]
+        accept_buffer = accept_types.split(",")
+        file_type = self.types_handler.get_type(suffix[1:])
 
-		accept_buffer = accept_types.split(",")
-		print("asd",suffix)
+        if file_type in accept_buffer:
+            return True
+        else:
+            for item in accept_buffer:
+                if item.startswith("*/*"):
+                    return True
+        return False
 
-		return True
+    def send_headers(self, client_socket, protocol, error_code, content_type=None, content_length = None, file_content=None):
+        error_html = "<html><body><h1>Error response</h1><p>Error code: {}</p><p>Message: {}.</p></body></html>\n"
+        server = "MyServer/1.0"
+        if not error_code.startswith('200'):
+            message = ""
+            if error_code.startswith('404'):
+                message = "File not found."
+            elif error_code.startswith('406'):
+                message = "File not acceptable"
+            else: #501
+                message = "The request method is not supported."
+            
+            content_type = "text/html"
+            file_content = error_html.format(error_code, message).encode("utf-8")
+            content_length = len(file_content)	
+        
+        output = "%s %s\nDate: %s\nServer: %s\nContent-Length: %d\nContent-Type: %s\n\n" % (protocol, error_code, time.asctime(), server, content_length, content_type) 
+        
+        if file_content:
+            client_socket.send(output.encode("utf-8")+file_content) 
+        else:
+            client_socket.send(output.encode("utf-8"))
+            
+    def log_info(self, command, server, referer='', url='', data=''):
+        logging.info(" {} > {} > {} > {} > /{} > {}".format(command, int(time.time()), server, referer, url, data))    
 
-		if accept_types == "*":
-			return True
-		elif accept_types != suffix:
-			return False
-		else: 
-			return True
+    def get_args(self, url):
+        long_url = urlparse(url)
+        file_name = long_url[2] #2: path index
+        query = long_url[4] #4: query index
 
-	def send_headers(self, client_socket, protocol, error_code, content_type=None, content_length = None, file_content=None):
-		error_html = "<html><body><h1>Error response</h1><p>Error code: {}</p><p>Message: {}.</p></body></html>\n"
-		server = "MyServer/1.0"
-		if not error_code.startswith('200'):
-			message = ""
-			if error_code.startswith('404'):
-				message = "File not found."
-			elif error_code.startswith('406'):
-				message = "File not acceptable"
-			else: #501
-				message = "The request method is not supported."
-			
-			content_type = "text/html"
-			file_content = error_html.format(error_code, message).encode("utf-8")
-			content_length = len(file_content)	
-		
-		output = "%s %s\nDate: %s\nServer: %s\nContent-Length: %d\nContent-Type: %s\n\n" % (protocol, error_code, time.asctime(), server, content_length, content_type) 
-		
-		if file_content:
-			client_socket.send(output.encode("utf-8")+file_content) 
-		else:
-			client_socket.send(output.encode("utf-8"))
-			
-	def log_info(self, command, server, referer='', url='', data=''):
-		logging.info(" {} > {} > {} > {} > /{} > {}".format(command, int(time.time()), server, referer, url, data))    
+        return file_name[1:], query
+        
+    def do_HEAD(self, data_vector, client_socket):
+        command = data_vector[0]
+        file_name = data_vector[1]
+        host_name = data_vector[2]
+        protocol = data_vector[4]
 
-	def get_args(self, url):
-		long_url = urlparse(url)
-		file_name = long_url[2] #2: path index
-		query = long_url[4] #4: query index
+        file_name, query = self.get_args(file_name)
+        self.log_info(command , host_name, '', file_name, query)
 
-		return file_name[1:], query
-		
-	def do_HEAD(self, data_vector, client_socket):
-		command = data_vector[0]
-		file_name = data_vector[1]
-		host_name = data_vector[2]
-		protocol = data_vector[4]
+        if self.check_file(file_name): 
+            if self.check_accept(data_vector):
+                file_content = open(file_name,'rb').read() # rb lo abre como binario para que las imagenes funcionen
+                _, suffix = os.path.splitext(file_name)
+                
+                content_type = self.types_handler.get_type(suffix[1:])
+                content_length = len(file_content)
 
-		file_name, query = self.get_args(file_name)
-		self.log_info(command , host_name, '', file_name, query)
+                self.send_headers(client_socket,protocol, "200 OK", content_type, content_length,None)
+            else:
+                self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)	
+        else:
+            self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
 
-		if self.check_file(file_name): 
-			if self.check_accept(data_vector):
-				file_content = open(file_name,'rb').read() # rb lo abre como binario para que las imagenes funcionen
-				_, suffix = os.path.splitext(file_name)
-				
-				content_type = self.types_handler.get_type(suffix[1:])
-				content_length = len(file_content)
+    def do_GET(self, data_vector, client_socket):
+        query = ''
+        command = data_vector[0]
+        file_name = data_vector[1]
+        host_name = data_vector[2]
+        protocol = data_vector[4]
 
-				self.send_headers(client_socket,protocol, "200 OK", content_type, content_length,None)
-			else:
-				self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)	
-		else:
-			self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
-	
-	def do_GET(self, data_vector, client_socket):
-		query = ''
-		command = data_vector[0]
-		file_name = data_vector[1]
-		host_name = data_vector[2]
-		protocol = data_vector[4]
+        if file_name == '/':
+            file_name = 'index.html'
+        else:
+            file_name, query = self.get_args(file_name)
 
-		if file_name == '/':
-			file_name = 'index.html'
-		else:
-			file_name, query = self.get_args(file_name)
+        self.log_info(command, host_name, '', file_name, query)
 
-		self.log_info(command, host_name, '', file_name, query)
+        if self.check_file(file_name):
+            if self.check_accept(data_vector):
+                file_content = open(file_name,'rb').read() # rb lo abre como binario para que las imagenes funcionen
 
-		if self.check_file(file_name):
-			if self.check_accept(data_vector):
-				file_content = open(file_name,'rb').read() # rb lo abre como binario para que las imagenes funcionen
+                _, suffix = os.path.splitext(file_name)
+                
+                content_type = self.types_handler.get_type(suffix[1:])
+                content_length = len(file_content)
 
-				_, suffix = os.path.splitext(file_name)
-				
-				content_type = self.types_handler.get_type(suffix[1:])
-				content_length = len(file_content)
+                self.send_headers(client_socket, protocol, "200 OK", content_type, content_length, file_content)
+            else:
+                self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)		
+        else:
+            self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
 
-				self.send_headers(client_socket, protocol, "200 OK", content_type, content_length, file_content)
-			else:
-				self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)		
-		else:
-			self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
-	
-	def do_POST(self, data_vector, client_socket):
-		command = data_vector[0]
-		file_name = data_vector[1]
-		host_name = data_vector[2]
-		protocol = data_vector[4]
-		
-		file_name, query = self.get_args(file_name)
-		self.log_info(command, host_name, '', file_name, query)
-		
-		try:
-			file_content = open(file_name,'rb').read()
-			if self.check_accept(data_vector):
-				_prefix, suffix = os.path.splitext(file_name)
-				
-				content_type = self.types_handler.get_type(suffix[1:])
-				content_length = len(file_content)
+    def do_POST(self, data_vector, client_socket):
+        command = data_vector[0]
+        file_name = data_vector[1]
+        host_name = data_vector[2]
+        protocol = data_vector[4]
+        
+        file_name, query = self.get_args(file_name)
+        self.log_info(command, host_name, '', file_name, query)
+        
+        try:
+            file_content = open(file_name,'rb').read()
+            if self.check_accept(data_vector):
+                _prefix, suffix = os.path.splitext(file_name)
+                
+                content_type = self.types_handler.get_type(suffix[1:])
+                content_length = len(file_content)
 
-				self.send_headers(client_socket, protocol, "200 OK", content_type, content_length, file_content)
-			else:
-				self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)	
-		except:
-			self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
+                self.send_headers(client_socket, protocol, "200 OK", content_type, content_length, file_content)
+            else:
+                self.send_headers(client_socket, protocol, "406 Not Acceptable",None,None,None)	
+        except:
+            self.send_headers(client_socket, protocol, "404 Not Found",None,None,None)
 
 class Request_Handler:
 	server = Server_Handler()
@@ -204,6 +203,7 @@ class Request_Handler:
 				return index
 			index+=1
 		return -1
+
 	def parse_request(self, data, client_socket):
 		buffer_vector = data.split()#la request como vector 
 
@@ -222,48 +222,47 @@ def run(data, request, client_socket):
 	new_data  = request.parse_request(data, client_socket)
 	request.call_server(new_data, client_socket)
 	
-
 def main():
-	port = 80
-	request = Request_Handler()
+    port = 80
+    request = Request_Handler()
 
-	server_socket = socket.socket()
-	server_socket.bind(('localhost',port))
-	server_socket.listen(5)
+    server_socket = socket.socket()
+    server_socket.bind(('localhost',port))
+    server_socket.listen(5)
 
-	print("Server running on port %s" % port)
-	
-	date_tag = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M-%S")
-	cwd = os.getcwd()
-	directory = os.path.join(cwd,"logs")
+    print("Server running on port %s" % port)
+    
+    date_tag = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M-%S")
+    cwd = os.getcwd()
+    directory = os.path.join(cwd,"logs")
 
-	if not os.path.exists(directory):
-		os.mkdir(directory)
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
-	file_name = '{}/logs/bitacora_{}.log'.format(cwd, date_tag)
-	logging.basicConfig(filename=file_name, level=logging.INFO)
+    file_name = '{}/logs/bitacora_{}.log'.format(cwd, date_tag)
+    logging.basicConfig(filename=file_name, level=logging.INFO)
 
-	client_socket = None
-	try:
-		while True:
-			client_socket, addr = server_socket.accept()
-			data = client_socket.recv(1024)
-			logging.info('\n---------\nLog started on %s.\n---------' % time.asctime())
-			if data:
-				process = Process(target=run, args=(data,request,client_socket,))
-				print("[%s] Started" % process.name)
-				process.start()
+    client_socket = None
+    try:
+        while True:
+            client_socket, addr = server_socket.accept()
+            data = client_socket.recv(1024)
+            
+            if data:
+                process = Process(target=run, args=(data,request,client_socket,))
+                print("[%s] Started" % process.name)
+                if process.name == "Process-1":
+                    logging.info('\n---------\nLog started on %s.\n---------' % time.asctime())
+                process.start()
 
-			if process.name != "Process-1":
-				process.join()			
-			
-	except KeyboardInterrupt:
-		print ("Closing web server")
-		logging.info('\n---------\nLog closed on %s.\n---------\n' % time.asctime())
-		client_socket.shutdown(SHUT_WR)
-		client_socket.close()
-	
-	
-
+            if process.name != "Process-1":
+                process.join()			
+            
+    except KeyboardInterrupt:
+        print ("Closing web server")
+        logging.info('\n---------\nLog closed on %s.\n---------\n' % time.asctime())
+        client_socket.shutdown(SHUT_WR)
+        client_socket.close()
+    
 if __name__ == '__main__':
     main()  
